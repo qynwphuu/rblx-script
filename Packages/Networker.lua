be55928c3ff4908dfa8175507ec619d7df046ed7
qynwphuu <qynwphuu@gmail.com>
2026-05-15 04:05:00 +0300

    install packages

--- Packages/Networker.lua
+++ Packages/Networker.lua
@@ -1,1 +1,4 @@
-return require(script.Parent._Index["leifstout_networker@0.3.1"]["networker"])
+local REQUIRED_MODULE = require(script.Parent._Index["leifstout_networker@0.3.1"]["networker"])
+export type Server = REQUIRED_MODULE.Server 
+export type Client = REQUIRED_MODULE.Client 
+return REQUIRED_MODULE
